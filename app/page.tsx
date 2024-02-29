57eeb1f3e08a7f5f230de6e71bbf9ded23c08dd9
alisonborba <alisonborbaa@gmail.com>
2024-02-29 15:57:53 +0000

    minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,14 @@ export default async function SignInPage() {
 
   return (
     <div className="w-full flex flex-col items-center justify-center min-h-screen py-2">
-      <div className="flex flex-col items-center mt-10 p-10 shadow-md">
+      <div className="flex flex-col items-center -mt-10 p-10 shadow-md">
         <h1 className="mt-10 mb-4 text-4xl font-bold">Sign In</h1>
         <GoogleSignInButton />
         <GithubSignInButton />
+        <pre>
+          experienced MERN Stack developer needed to build a cutting-edge chat
+          application (POC)
+        </pre>
         {/* <span className="text-2xl font-semibold text-white text-center mt-8">
           Or
         </span> */}
